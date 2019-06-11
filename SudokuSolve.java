f42a5ac740d8504b90b8a8eccdf69c6edae62eab
DaPinkOne <crystalshade@gmail.com>
2019-06-11 12:06:41 -0700

    removed commented/refactored out code

--- SudokuSolve.java
+++ SudokuSolve.java
@@ -168,128 +168,16 @@ public class SudokuSolve {
 		 * Sub-Square 2. tryNum is not a duplicate in Col (horizontal) 3. tryNum is not
 		 * a duplicate in Row (vertical) Return false above cases.
 		 */
-		// TODO: refactor to use later-dev'd infrastructure? This whole thing
-		// is magic.
 		for(Quadrant quad : quads) {
 			// if cell in quadrant, and the quadrant contains the value, return false.
 			if(determineQuadrant(row, col) == quad && quad.contains(board, tryNum)) return false;
 		}
-		
-//		// 1st square --> top-left
-//		if (row <= 2 && col <= 2) {// 1b
-//			for (int r1 = 0; r1 < 3; r1++) {
-//				for (int c1 = 0; c1 < 3; c1++) {
-//					if (board[r1][c1] == tryNum) {
-//						return false;
-//					}
-//				}
-//			}
-//		}
-//		// 2nd square --> top-middle
-//		if (row <= 2 && col > 2 && col <= 5) {
-//			for (int r2 = 0; r2 < 3; r2++) {
-//				for (int c2 = 3; c2 < 6; c2++) {
-//					if (board[r2][c2] == tryNum) {
-//						return false;
-//					}
-//				}
-//			}
-//		}
-//		// 3rd square --> top-right
-//		if (row <= 2 && col > 5) {
-//			for (int r3 = 0; r3 < 3; r3++) {
-//				for (int c3 = 6; c3 < 9; c3++) {
-//					if (board[r3][c3] == tryNum) {
-//						return false;
-//					}
-//				}
-//			}
-//
-//		}
-//		// 4th square --> middle-left
-//		if (row > 2 && row <= 5 && col <= 2) {
-//			for (int r4 = 3; r4 < 6; r4++) {
-//				for (int c4 = 0; c4 < 3; c4++) {
-//					if (board[r4][c4] == tryNum) {
-//						return false;
-//					}
-//				}
-//			}
-//
-//		}
-//		// 5th square --> middle-middle
-//		if (row > 2 && row <= 5 && col > 2 && col <= 5) {
-//			for (int r5 = 3; r5 < 6; r5++) {
-//				for (int c5 = 3; c5 < 6; c5++) {
-//					if (board[r5][c5] == tryNum) {
-//						return false;
-//					}
-//				}
-//			}
-//		}
-//		// 6th square --> middle-right
-//		if (row > 2 && row <= 5 && col > 5) {
-//			for (int r6 = 3; r6 < 6; r6++) {
-//				for (int c6 = 6; c6 < 9; c6++) {
-//					if (board[r6][c6] == tryNum) {
-//						return false;
-//					}
-//				}
-//			}
-//
-//		}
-//		// 7th square --> bottom-left
-//		if (row > 5 && col <= 2) {
-//			for (int r7 = 6; r7 < 9; r7++) {
-//				for (int c7 = 0; c7 < 3; c7++) {
-//					if (board[r7][c7] == tryNum) {
-//						return false;
-//					}
-//				}
-//			}
-//
-//		}
-//		// 8th square --> bottom-middle
-//		if (row > 5 && col > 2 && col <= 5) {
-//			for (int r8 = 6; r8 < 9; r8++) {
-//				for (int c8 = 3; c8 < 6; c8++) {
-//					if (board[r8][c8] == tryNum) {
-//						return false;
-//					}
-//				}
-//			}
-//
-//		}
-//		// 9th square --> bottom-right
-//		if (row > 5 && col > 5) {
-//			for (int r9 = 6; r9 < 9; r9++) {
-//				for (int c9 = 6; c9 < 9; c9++) {
-//					if (board[r9][c9] == tryNum) {
-//						return false;
-//					}
-//				}
-//			}
-//		}
 
-		// 2. check col (horizontally for duplicate)
+		// 2. check column for the number we want to place, tryNum
 		if(columnContains(board, col, tryNum)) return false;
-//		for (int rowChecker = row; rowChecker < row + 1; rowChecker++) {
-//			for (int horiChecker = 0; horiChecker < 9; horiChecker++) {
-//				if (board[rowChecker][horiChecker] == tryNum) {
-//					return false;
-//				}
-//			}
-//		}
 
 		// 3. check row (vertically for duplicate)
 		if(rowContains(board, row, tryNum)) return false;
-//		for (int colChecker = col; colChecker < col + 1; colChecker++) {
-//			for (int vertiChecker = 0; vertiChecker < 9; vertiChecker++) {
-//				if (board[vertiChecker][colChecker] == tryNum) {
-//					return false;
-//				}
-//			}
-//		}
 
 		// At this point, I should add the tryNum to the board.
 		board[row][col] = tryNum;
